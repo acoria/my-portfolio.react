f43e209b9311694e9fa8d87364122b03ff7dcebe
acoria <tewes.verena@gmail.com>
2024-08-02 14:19:23 +0200

    Add fonts and transition of header texts

--- src/features/aboutMe/AboutMe.tsx
+++ src/features/aboutMe/AboutMe.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { texts } from "../../hooks/useTranslation/texts";
+import { useTranslation } from "../../hooks/useTranslation/useTranslation";
 import styles from "./AboutMe.module.scss";
 export const AboutMe: React.FC = () => {
+  const { t } = useTranslation();
+
+  
+
+  useEffect(()=>{},[])
+
   return (
     <div className={styles.aboutMe}>
-      <h1 style={{ margin: 0 }}>About me</h1>
+      <img
+        src="./assets/Portrait_Verena_Tewes.jpg"
+        alt={t(texts.aboutMe.portraitImageDescription)}
+        className={styles.portrait}
+      />
+      <div>
+        <h1 className={styles.firstHeadline}>{t(texts.aboutMe.headline_1)}</h1>
+        <h1 className={styles.secondHeadline}>{t(texts.aboutMe.headline_2)}</h1>
+        <h1 className={styles.thirdHeadline}>{t(texts.aboutMe.headline_3)}</h1>
+      </div>
     </div>
   );
 };
