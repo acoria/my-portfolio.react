ac422067caacdd3914ecc7eab4b5cbabd705accc
acoria <tewes.verena@gmail.com>
2024-08-30 11:17:02 +0200

    Add TechStack

---
+++ src/features/projects/projectDetails/techStack/ITechStackProps.ts
@@ -0,0 +1,5 @@
+import { ITechnology } from "../../../../shared/model/ITechnology";
+
+export interface ITechStackProps {
+  technologies: ITechnology[];
+}
---
+++ src/features/projects/projectDetails/techStack/TechStack.tsx
@@ -0,0 +1,8 @@
+import { ITechStackProps } from "./ITechStackProps";
+import styles from "./TechStack.module.scss";
+
+export const TechStack: React.FC<ITechStackProps> = (props) => {
+  return <div className={styles.techStack}>
+    
+  </div>;
+};
