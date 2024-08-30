95ebc9284c4e7cb336f8f803edd3b9a43a4c85ce
acoria <tewes.verena@gmail.com>
2024-08-30 10:16:49 +0200

    Add SNP logo

--- src/features/projects/projectDetails/customer/Customer.tsx
+++ src/features/projects/projectDetails/customer/Customer.tsx
@@ -4,9 +4,18 @@ import styles from "./Customer.module.scss";
 export const Customer: React.FC<ICustomerProps> = (props) => {
   return (
     <div className={styles.customer}>
-      <h1 className={styles.name}>{props.customer.name}</h1>
-      <div>{props.customer.branch}</div>
-      <div>{props.customer.employeeSize}</div>
+      {props.customer.logoUrl && (
+        <img
+          alt="company logo"
+          src={props.customer.logoUrl}
+          className={styles.logo}
+        />
+      )}
+      <div>
+        <h1 className={styles.name}>{props.customer.name}</h1>
+        <div>{props.customer.branch}</div>
+        <div>{props.customer.employeeSize}</div>
+      </div>
     </div>
   );
 };
--- src/shared/model/ICustomer.ts
+++ src/shared/model/ICustomer.ts
@@ -1,4 +1,5 @@
 export interface ICustomer {
+  logoUrl?: string;
   name: string;
   branch: string;
   employeeSize: string;
