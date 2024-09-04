b710bf1b8500ff89e87c0ef5d2e815db81cd8284
acoria <tewes.verena@gmail.com>
2024-09-04 15:47:40 +0200

    Add all technologies for projects

--- src/hooks/useTechnologyTypeName.ts
+++ src/hooks/useTechnologyTypeName.ts
@@ -8,22 +8,26 @@ export const useTechnologyTypeName = (): ((
 ) => string) => {
   const { t } = useTranslation();
 
-  const convertTechnologyTypeName = (technologyType: TechnologyType): string => {
+  const convertTechnologyTypeName = (
+    technologyType: TechnologyType
+  ): string => {
     switch (technologyType) {
       case TechnologyType.COMMUNICATION_FRAMEWORKS:
         return t(texts.technologies.types.communication_frameworks);
-      case TechnologyType.PROGRAMMING_LANGUAGES:
-        return t(texts.technologies.types.programming_languages);
       case TechnologyType.DATABASES:
         return t(texts.technologies.types.databases);
       case TechnologyType.FRAMEWORKS_CONCEPTS:
         return t(texts.technologies.types.frameworks_concepts);
+      case TechnologyType.OTHERS:
+        return t(texts.technologies.types.others);
+      case TechnologyType.PROGRAMMING_LANGUAGES:
+        return t(texts.technologies.types.programming_languages);
+      case TechnologyType.PROJECT_MANAGEMENT:
+        return t(texts.technologies.types.project_management);
       case TechnologyType.TESTING:
         return t(texts.technologies.types.testing);
       case TechnologyType.TOOLS:
         return t(texts.technologies.types.tools);
-      case TechnologyType.PROJECT_MANAGEMENT:
-        return t(texts.technologies.types.project_management);
       default:
         error(
           `Missing technology type "${technologyType}". This type is not mapped to a name.`
