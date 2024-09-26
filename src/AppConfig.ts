3e7fb705ef71ceda30acb81ca0eaf403b4d21ccb
acoria <tewes.verena@gmail.com>
2024-09-26 10:13:45 +0200

    Add social media links for desktop view

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -7,4 +7,12 @@ export const AppConfig = {
   MY_EMAIL:
     process.env.REACT_APP_MY_EMAIL ??
     error("Error while getting e-mail from environment variables"),
+  PROFILE_LINK_XING:
+    process.env.REACT_APP_PROFILE_LINK_XING ??
+    error("Error while getting Xing profile link from environment variables"),
+  PROFILE_LINK_LINKEDIN:
+    process.env.REACT_APP_PROFILE_LINK_LINKEDIN ??
+    error(
+      "Error while getting LinkedIn profile link from environment variables"
+    ),
 };
--- src/features/socialMedia/SocialMedia.tsx
+++ src/features/socialMedia/SocialMedia.tsx
@@ -1,21 +1,31 @@
-import styles from "./SocialMedia.module.scss";
+import { AppConfig } from "../../AppConfig";
 import { ReactComponent as XingIcon } from "../../assets/icons/xingIcon.svg";
-import { useTranslation } from "../../hooks/useTranslation/useTranslation";
-import { texts } from "../../hooks/useTranslation/texts";
-import { ISocialMediaProps } from "./ISocialMediaProps";
+import { Button } from "../../components/button/Button";
 import { style } from "../../core/utils/style";
+import { texts } from "../../hooks/useTranslation/texts";
+import { useTranslation } from "../../hooks/useTranslation/useTranslation";
+import { ISocialMediaProps } from "./ISocialMediaProps";
+import styles from "./SocialMedia.module.scss";
 
 export const SocialMedia: React.FC<ISocialMediaProps> = (props) => {
   const { t } = useTranslation();
 
   return (
     <div className={style(styles.socialMedia, props.className)}>
-      <img
-        src="./assets/icons/linkedIn.png"
-        alt={t(texts.socialMedia.linkedInIconDescription)}
-        className={styles.linkedInIcon}
-      />
-      <XingIcon className={styles.xingIcon} />
+      <a
+        href={AppConfig.PROFILE_LINK_LINKEDIN}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="./assets/icons/linkedIn.png"
+          alt={t(texts.socialMedia.linkedInIconDescription)}
+          className={styles.linkedInIcon}
+        />
+      </a>
+      <a href={AppConfig.PROFILE_LINK_XING} target="_blank" rel="noreferrer">
+        <XingIcon />
+      </a>
     </div>
   );
 };
